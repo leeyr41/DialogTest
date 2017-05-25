e7bf0260c6f955fa564641429256d8fbd570309c
leeyr41 <leeyr41@naver.com>
2017-05-25 20:50:37 +0900

    second commit

--- app/src/main/java/leeyr41/dialogtest/MainActivity.java
+++ app/src/main/java/leeyr41/dialogtest/MainActivity.java
@@ -1,12 +1,15 @@
 package leeyr41.dialogtest;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] itemArr={"소원", "예린", "유주", "은하", "신비", "엄지"};
+    Button butDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        //dialog.setMessage("여기는 메게지를 쓰는 곳입니다.");
+        dialog.setItems(itemArr, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                butDialog.setText(itemArr[i]);
+            }
+        });
+        dialog.setPositiveButton("OK", null);
+        dialog.show();
     }
 }
